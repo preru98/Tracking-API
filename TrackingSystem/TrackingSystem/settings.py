0a06be826ce72220c841dd882567aeae71967077
Prerna <prerna1998.ps@gmail.com>
2020-01-18 01:39:31 +0530

    credent

--- TrackingSystem/TrackingSystem/settings.py
+++ TrackingSystem/TrackingSystem/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'TrackingSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'record',
-        'USER': 'mysuperuser',
-        'PASSWORD': 'mysuperuserpass',
-        'HOST': 'localhost',
+        'NAME': 'ddtonc9dn9c2ta',
+        'USER': 'otkbteyxftbecv',
+        'PASSWORD': '71d71dfde8a6df0a46e9c981f5886717b147c359b36e9480687d3e266f52b90b',
+        'HOST': 'ec2-23-23-92-204.compute-1.amazonaws.com',
         'PORT': '',
     }
 }
